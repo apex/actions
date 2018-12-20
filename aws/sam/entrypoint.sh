4e3bbb6ee5032101481283818bb744c7aa9942a8
TJ Holowaychuk <tj@vision-media.ca>
2018-12-20 18:20:05 +0000

    aws/sam: add Slack support

--- aws/sam/entrypoint.sh
+++ aws/sam/entrypoint.sh
@@ -9,6 +9,7 @@ set -e
 [ -n "$AWS_DEFAULT_OUTPUT" ] || export AWS_DEFAULT_OUTPUT=json
 
 # Capture output
+start=$SECONDS
 output=$( sh -c "sam $*" )
 
 # Preserve output for consumption by downstream actions
@@ -16,3 +17,23 @@ echo "$output" > "${HOME}/${GITHUB_ACTION}.${AWS_DEFAULT_OUTPUT}"
 
 # Write output to STDOUT
 echo "$output"
+
+# Slack message
+duration=$(($SECONDS - $start))
+icon="https://newmathdata.com/wp-content/uploads/2018/03/aws_sam_local.png"
+title=$GITHUB_REPOSITORY
+text="$1 completed"
+footer="Completed in ${duration}s"
+
+cat <<EOF > slack.json
+{
+  "icon_url": $icon,
+  "attachments": [
+    {
+      "title": "$title",
+      "text": "$text",
+      "footer": "$footer"
+    }
+  ]
+}
+EOF
